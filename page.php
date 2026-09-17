3d1170aa5fef07dd6d69a8998643617484985d64
Saikomantisu <rajaravinath0@gmail.com>
2026-09-18 01:17:31 +0530

    added page.php to render static pages

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<div class="content-area">
+    <?php while (have_posts()) : the_post(); ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+            <header class="page-header">
+                <h1><?php the_title(); ?></h1>
+            </header>
+
+            <div class="page-content">
+                <?php the_content(); ?>
+            </div>
+
+        </article>
+    <?php endwhile; ?>
+</div>
+
+<?php get_footer(); ?>
