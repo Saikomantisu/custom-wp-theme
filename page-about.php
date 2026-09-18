3e8a8b64dcc1ba12e11d1c7b7ad7cccdc38c417e
Saikomantisu <rajaravinath0@gmail.com>
2026-09-18 16:21:25 +0530

    add page-about.php

---
+++ page-about.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="content-area about-page">
+    <header class="page-header">
+        <h1>About Us</h1>
+        <p>Learn more about our company.</p>
+    </header>
+
+    <div class="page-content">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum sed, odio similique praesentium suscipit non totam magni nisi fugit, iusto delectus harum dolores?</p>
+    </div>
+</div>
+
+<?php get_footer(); ?>
